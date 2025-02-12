a9cf14cf4d39fbcd9cce6b3b2a3e0028ed31a173
mjl <2404816298@qq.com>
2025-02-12 16:29:55 +0800

    引入uview-plus

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
-import { createSSRApp } from "vue";
-import App from "./App.vue";
+import { createSSRApp } from 'vue';
+import App from './App.vue';
+import uviewPlus, { setConfig } from 'uview-plus';
 export function createApp() {
   const app = createSSRApp(App);
+  app.use(uviewPlus);
+  setConfig({
+    config: {
+      unit: 'rpx',
+    },
+  });
   return {
     app,
   };
